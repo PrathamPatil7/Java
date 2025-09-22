7f9327090ebe4f849085aecbce378e07717eaca6
Prathamesh Patil <prathameshvpatil916@gmail.com>
2025-09-23 00:00:45 +0530

    pattern

---
+++ Hollow_Rectangle.java
@@ -0,0 +1,25 @@
+public class Hollow_Rectangle{
+    public static void hollowReactangle(int n){
+        for(int i=1; i<=n; i++){
+            //space
+            for(int j=1; j<=(n-i); j++){
+                System.out.print(" ");
+            }
+
+            //Boundary condition
+            for(int j=1; j<=n; j++){
+                if(i == 1 || i == n || j==1 || j==n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        hollowReactangle(5);
+    }
+}
