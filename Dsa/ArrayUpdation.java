9f496d1f41a4d3341231cdc2ca88e0ef2157d01c
Prathamesh Patil <prathameshvpatil916@gmail.com>
2025-09-28 00:47:11 +0530

    updation in array

---
+++ Dsa/ArrayUpdation.java
@@ -0,0 +1,11 @@
+public class ArrayUpdation{
+    public static void main(String[] args) {
+        int marks[] = new int[5];
+
+        marks[2]=100;
+        System.out.println("Your math marks are:"+marks[2]);
+
+        marks[2]=400;
+        System.out.println("Your math marks are:"+marks[2]);
+    }
+}
