a43168af7433f6f20ae0cd0120d3faf707b8ae7f
Prathamesh Patil <prathameshvpatil916@gmail.com>
2025-09-28 00:11:57 +0530

    how to write array

---
+++ Dsa/ArrayBasic.java
@@ -0,0 +1,11 @@
+public class ArrayBasic{
+    public static void main(String[] args) {
+        int Marks[] = new int[50];  //one way to write array
+
+        int Numbers[] = {1,2,3};   //another way to write array
+
+        int MoreNumber[] = {4,5,6};
+
+        String fruits[] = {"Apple","Orange","Bannana","Mango"};
+    }
+}
